c244908b48791a29a142e2e9ae8e36228d43abd4
credding22 <credding22@8eea147d-c515-40c2-9308-a1fce190b8fa>
2009-11-10 16:28:21 +0000

    A Box which displays a keyboard on the canvas.

--- src/ampt/ui/canvas/KeyboardBox.java
+++ src/ampt/ui/canvas/KeyboardBox.java
@@ -1,40 +1,126 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ampt.ui.canvas;
 
+import ampt.ui.keyboard.KeyboardDevice;
 import ampt.ui.keyboard.KeyboardPanel;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import javax.swing.JComponent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
- *
+ * This class extends MidiDeviceBox in order to have the keyboard and its
+ * properties be part of the canvas box.
  * @author Christopher
  */
-public class KeyboardBox extends JComponent {
+public class KeyboardBox extends MidiDeviceBox implements ChangeListener, ActionListener{
 
-    private static final String TEXT = "Keyboard";
-    private final KeyboardPanel keyboard;
+    private static final int VELOCITY_MIN = 0;
+    private static final int VELOCITY_MAX = 127;
+    private static final int STARTING_VELOCITY = 64;
 
-    public KeyboardBox(KeyboardPanel keyboard){
-        this.keyboard = keyboard;
-        this.setPreferredSize(new Dimension(51, 51));
+    private JSlider velocitySlider;
+    private JComboBox channelComboBox;
+    private JComboBox octaveComboBox;
+
+    /**
+     * Constructor to create the box.
+     * @param device
+     */
+    public KeyboardBox(KeyboardDevice device){
+        super(device);
+        // We need to change the preferred size to null since it was changed in
+        // MidiDeviceBox.  This way, the components will determine the size of 
+        // this panel.
+        this.setPreferredSize(null);
+        overridePaintComponent = false;
+
+        this.setLayout(new BorderLayout());
+        this.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Software Keyboard", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.BELOW_TOP));
+        this.setBackground(Color.CYAN);
+
+        // Make the center panel contain the keyboard
+        JPanel centerPanel = new JPanel();
+        KeyboardPanel keyboardPanel = new KeyboardPanel(device);
+        centerPanel.add(keyboardPanel);
+        centerPanel.setBackground(Color.CYAN);
+        this.add(centerPanel, BorderLayout.CENTER);
+
+
+
+        // Make a south panel to contain the velocity slider
+        JPanel southPanel = new JPanel();
+        southPanel.setBackground(Color.CYAN);
+        velocitySlider = new JSlider(VELOCITY_MIN, VELOCITY_MAX, STARTING_VELOCITY);
+        device.setVelocity(STARTING_VELOCITY);
+        velocitySlider.addChangeListener(this);
+        velocitySlider.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Velocity"));
+        southPanel.add(velocitySlider);
+        this.add(southPanel, BorderLayout.SOUTH);
+
+        // Make an east panel to contain the channel and octave selecters
+        JPanel eastPanel = new JPanel();
+        eastPanel.setBackground(Color.CYAN);
+        eastPanel.setLayout(new BoxLayout(eastPanel, BoxLayout.Y_AXIS));
+        Integer[] channels = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        channelComboBox = new JComboBox(channels);
+        channelComboBox.setSelectedIndex(0);
+        device.setChannel(channels[0]);
+        channelComboBox.addActionListener(this);
+        channelComboBox.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Chan."));
+        eastPanel.add(channelComboBox);
+
+        Integer[] octaves = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        octaveComboBox = new JComboBox(octaves);
+        octaveComboBox.setSelectedIndex(5);
+        device.setOctave(octaves[5]);
+        octaveComboBox.addActionListener(this);
+        octaveComboBox.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Octave"));
+        eastPanel.add(octaveComboBox);
+
+        this.add(eastPanel, BorderLayout.EAST);
+
+
+//        this.add(southPanel, BorderLayout.SOUTH);
+
     }
 
+    /**
+     * Listener for velocity slider
+     * @param e
+     */
     @Override
-    protected void paintComponent(Graphics g) {
-    	Color color = g.getColor();
-	g.setColor(Color.BLACK);
+    public void stateChanged(ChangeEvent e) {
+        if(e.getSource().equals(velocitySlider)){
+            KeyboardDevice keyboard = (KeyboardDevice) midiDevice;
+            keyboard.setVelocity(velocitySlider.getValue());
+        }
+    }
 
-        g.drawRect(0, 0, 50, 50);
+    /**
+     * Listener for channel combo box and octave combo box
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(channelComboBox)){
+            KeyboardDevice keyboard = (KeyboardDevice) midiDevice;
+            keyboard.setChannel((Integer)channelComboBox.getSelectedItem() - 1);
+        }
+        else if(e.getSource().equals(octaveComboBox)){
+            KeyboardDevice keyboard = (KeyboardDevice) midiDevice;
+            keyboard.setOctave((Integer) octaveComboBox.getSelectedItem());
+        }
+    }
 
-	g.drawString(TEXT, 5, 45);
 
-	g.setColor(color);
-	}
 
 }
