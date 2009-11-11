01dc3319fd307196a0e0ae58688856b829d92f46
avazant <avazant@8eea147d-c515-40c2-9308-a1fce190b8fa>
2009-11-11 01:02:25 +0000

    Added a timing test that allows you to select the output port

--- trunk/src/ampt/examples/timing/SoundTimingTest.java
+++ trunk/src/ampt/examples/timing/SoundTimingTest.java
@@ -1,30 +1,29 @@
 package ampt.examples.timing;
 
 import ampt.core.time.Clock;
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Receiver;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Synthesizer;
 
 /**
- *
+ * This test plays a scale to a given receiver. See HarwareTimingTest for a
+ * user interface to this test.
+ * 
  * @author Ben
  */
 public class SoundTimingTest {
 
-    public static void main(String[] args) throws Exception {
+    public void playNotesAtInterval(Receiver rcvr, int midiChannel, int noteInterval, int noteLength) throws InvalidMidiDataException {
 
-                
-        Synthesizer synth = MidiSystem.getSynthesizer();
-        synth.open();
-        Receiver rcvr = synth.getReceiver();
         ShortMessage msg1 = new ShortMessage();
         ShortMessage msg2 = new ShortMessage();
+
         int note = 59;
         int sign = 1;
-        msg1.setMessage(ShortMessage.NOTE_ON, 7, note, 63);
-        msg2.setMessage(ShortMessage.NOTE_OFF, 7, note, 63);
-
+        msg1.setMessage(ShortMessage.NOTE_ON, midiChannel, note, 63);
+        msg2.setMessage(ShortMessage.NOTE_OFF, midiChannel, note, 63);
 
         long natElapsed = 0;
         long last = 0;
@@ -34,13 +33,12 @@ public class SoundTimingTest {
 
         boolean noteOn = false;
 
-        while(natElapsed < 100000) {
+        while (natElapsed < 100000) {
 
             natElapsed = clock.getElapsedTime(Clock.UNIT_MILLIS);
 
-
             // turn note on
-            if(!noteOn && natElapsed-last >= 120) {
+            if (!noteOn && natElapsed - last >= noteInterval) {
 
                 last = natElapsed;
 
@@ -56,11 +54,11 @@ public class SoundTimingTest {
             }
 
             // turn note off
-            if(noteOn && natElapsed-last >= 60) {
+            if (noteOn && natElapsed - last >= noteLength) {
                 rcvr.send(msg2, -1);
                 noteOn = false;
 
-                if(note > 71) {
+                if (note > 71) {
                     sign = -1;
                 } else if (note < 59) {
                     sign = 1;
@@ -68,11 +66,22 @@ public class SoundTimingTest {
                 note += sign;
 
 
-                msg1.setMessage(ShortMessage.NOTE_ON, 7, note, 63);
-                msg2.setMessage(ShortMessage.NOTE_OFF, 7, note, 63);
+                msg1.setMessage(ShortMessage.NOTE_ON, midiChannel, note, 63);
+                msg2.setMessage(ShortMessage.NOTE_OFF, midiChannel, note, 63);
             }
 
-
         }
+    }
+
+    
+    public static void main(String[] args) throws Exception {
+
+        Synthesizer synth = MidiSystem.getSynthesizer();
+        synth.open();
+        Receiver rcvr = synth.getReceiver();
+
+        SoundTimingTest test = new SoundTimingTest();
+        test.playNotesAtInterval(rcvr, 7, 120, 60);
+
     }
 }
