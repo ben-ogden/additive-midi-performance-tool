9fdeeb2cb062a2697a77cff6fb4fe0eccd35f35b
Ben Ogden <ben.ogden@gmail.com>
2009-11-14 19:24:16 +0000

    Removed connectTo from interface

--- src/ampt/core/devices/AmptMidiDevice.java
+++ src/ampt/core/devices/AmptMidiDevice.java
@@ -1,7 +1,6 @@
 package ampt.core.devices;
 
 import javax.sound.midi.MidiDevice;
-import javax.sound.midi.MidiUnavailableException;
 
 /**
  * All AmptDevices should implement AmptMidiDevice.
@@ -10,13 +9,6 @@ import javax.sound.midi.MidiUnavailableException;
  */
 public interface AmptMidiDevice extends MidiDevice {
 
-    /**
-     * Connect a transmitter (output) from this MidiDevice to a receiver (input)
-     * of the given MidiDevice.
-     *
-     * @param device the MidiDevice to connect to
-     * @throws MidiUnavailableException if the connection cannot be made
-     */
-    public void connectTo(MidiDevice device) throws MidiUnavailableException;
+
 
 }
