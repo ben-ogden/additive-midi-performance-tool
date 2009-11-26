6dd67af9dfb5be8e117d454517f6fac49cdc7d40
avazant <avazant@8eea147d-c515-40c2-9308-a1fce190b8fa>
2009-11-26 00:56:50 +0000

    Added toString

--- trunk/src/ampt/midi/chord/ChordType.java
+++ trunk/src/ampt/midi/chord/ChordType.java
@@ -6,16 +6,19 @@ package ampt.midi.chord;
  */
 public enum ChordType {
 
-    MAJOR(4, 7),
-    MINOR(3, 7),
-    AUGMENTED(3, 6),
-    DIMINISHED(4, 8);
+    MAJOR(4, 7, "Major"),
+    MINOR(3, 7, "Minor"),
+    AUGMENTED(3, 6, "Augmented"),
+    DIMINISHED(4, 8, "Diminished");
+
     private final int thirdInterval;
     private final int fifthInterval;
+    private final String name;
 
-    ChordType(int third, int fifth) {
+    ChordType(int third, int fifth, String name) {
         this.thirdInterval = third;
         this.fifthInterval = fifth;
+        this.name = name;
     }
 
     public int getThirdInterval() {
@@ -25,4 +28,9 @@ public enum ChordType {
     public int getFifthInterval() {
         return fifthInterval;
     }
+
+    public String toString() {
+        return name;
+    }
+    
 };
